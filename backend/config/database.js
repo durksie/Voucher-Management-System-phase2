d90a889ce4ab1e1394901bfa56290acf4312bd73
Durksie <durksie1@gmail.com>
2025-11-07 14:54:32 +0200

    updated database

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise")
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
+  password: process.env.DB_PASSWORD || "#Nyiko1@",
   database: process.env.DB_NAME || "voucher_db",
   waitForConnections: true,
   connectionLimit: 10,
